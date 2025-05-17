e43885824964f2d24abeef74490520ce1d2e8b95
Aryan Pillai <aryanpillai518@gmail.com>
2025-05-18 00:21:18 +0530

    main changed

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AssetManagementModule);
   app.useGlobalInterceptors(new BigIntInterceptor());
   app.enableCors({
-    origin: 'https://invenso-frontend-hh5z.vercel.app', // or your deployed frontend URL
+    origin:
+      'https://invenso-frontend-hh5z.vercel.app',
+     // or your deployed frontend URL
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type, x-admin-token',
   });
